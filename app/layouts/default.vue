c323d817c747b29834b456f88db959621e94f207
itsmelouis <me@itslouis.dev>
2026-02-06 16:24:23 +0100

    fix: improve layout structure for no more scroll

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen">
     <AppHeader />
-    <main class="min-h-screen px-8 pt-24 pb-8">
+    <main class="flex-1 px-8 pt-24 pb-8">
       <slot />
     </main>
   </div>
